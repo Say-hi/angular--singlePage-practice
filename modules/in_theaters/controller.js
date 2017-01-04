25197af9c0db278247e90598a5cdab2118fa34e6
jwq <jiangwenqiang@jwq.com>
2017-01-04 17:31:35 +0800

    '正在热映页面完成'

--- modules/in_theaters/controller.js
+++ modules/in_theaters/controller.js
@@ -4,13 +4,25 @@
 // 正在热映子模块
 (function (angular) {
     var app = angular.module("mainApp.in_theaters",[]);
-    app.controller('in_theatersCtrl',['$scope','httpService',function ($scope,httpService) {
+    app.controller('in_theatersCtrl',['$scope','httpService','$route','$routeParams',function ($scope,httpService,$route,$routeParams) {
+        $scope.count=5;
+        $scope.currentPage=$routeParams.page||1;
+        $scope.pageChange=function (page) {
+            if(page>1&&page<=$scope.allPage){
+                //更新参数内的值
+                $route.updateParams({page:page});
+            }
+        }
         // 调用服务
         httpService.getJSONP('http://api.douban.com/v2/movie/in_theaters',{
-            // apikey:
+            start:($scope.currentPage-1)*$scope.count,
+            count:$scope.count,
+            apikey:'00fa6c0654689a0202ef4412fd39ce06'
         },function (data) {
-            $scope.data=data;
+            $scope.total=data.total;
+            $scope.data=data.subjects;
             $scope.title=data.title;
+            $scope.allPage=Math.ceil(data.total/$scope.count);
             //手动通知angular改变参数
             $scope.$apply();
         })
